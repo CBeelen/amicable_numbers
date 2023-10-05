3e8601728cc616a2943d3be40e5fec3669b3b87e
CBeelen <cha.beelen@gmail.com>
2023-10-05 15:02:52 +0200

    Add a first unit test

---
+++ tests/test_amicable.py
@@ -0,0 +1,11 @@
+import unittest
+
+from amicable_numbers import get_divisors
+
+
+class DivisorTests(unittest.TestCase):
+    def test_divisors(self):
+        n = 50
+        expected_divisors = [1, 2, 5, 10, 25]
+
+        self.assertListEqual(get_divisors(n), expected_divisors)
